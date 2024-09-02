90bcb9f36ec7e399f11b67ce9f58b8ed1639368c
headevs <106855879+eduardodfran@users.noreply.github.com>
2024-09-02 19:35:28 +0800

    Update signup.js

--- client/public/signup.js
+++ client/public/signup.js
@@ -12,7 +12,7 @@ document.getElementById('signupButton').addEventListener('click', async () => {
     }
 
     try {
-        const response = await fetch('http://localhost:3000/api/auth/signup', {
+        const response = await fetch('https://studelist-app-api.vercel.app/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
